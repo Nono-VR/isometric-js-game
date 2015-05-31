28c909eb9df042e6ebe4052b5c555cf95d62c631
kegeenkhg <kegeenkhg@gmail.com>
2015-05-31 12:31:43 -0400

    0.0.1

---
+++ isometric.js
@@ -0,0 +1,847 @@
+var layer0 = [
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,2],
+	[2,2,2,2,2,2,2,2,2,2,2,2,3],
+	[2,2,2,2,2,2,2,2,2,2,2,2,3],
+	[1,2,2,2,2,2,2,2,2,2,2,3,3],
+	[1,1,2,2,2,2,2,2,2,2,3,3,3],
+	[1,1,2,2,3,1,2,2,2,2,3,3,3],
+	[1,1,1,2,3,3,1,2,2,2,3,3,3],
+	[1,1,1,3,3,3,1,2,2,3,3,3,2],
+	[1,1,1,1,3,3,3,1,2,3,3,3,2],
+	[1,1,1,1,3,3,3,1,3,3,3,2,2],
+	[1,1,1,1,1,3,3,3,3,3,3,2,2],
+	[1,1,1,1,1,3,3,3,3,3,2,2,2],
+	[1,1,1,1,1,1,3,3,3,3,2,2,2],
+	[1,1,1,1,1,1,3,3,3,2,2,2,2],
+	[1,1,1,1,1,1,3,3,3,2,2,2,2],
+	[1,1,1,1,1,3,3,3,3,2,2,2,2],
+	[1,1,1,1,1,3,3,3,3,3,2,2,2],
+	[1,1,1,1,3,3,3,3,3,3,2,2,2],
+	[1,1,1,1,1,3,3,3,3,3,2,2,2],
+	[1,1,1,1,1,3,3,3,3,2,2,2,1],
+	[1,1,1,1,1,1,3,3,3,2,2,2,1],
+	[1,1,1,1,1,3,3,3,3,1,2,1,1],
+	[1,1,1,1,1,3,3,3,3,3,1,1,1],
+	[1,1,1,1,1,3,3,3,3,3,1,1,1],
+	[1,1,1,1,1,1,3,3,3,3,3,1,1],
+	[1,1,1,1,1,1,1,3,3,3,3,1,1],
+	[1,1,1,1,1,1,1,1,3,3,3,3,1],
+	[1,1,1,1,1,1,1,1,3,3,3,3,1],
+	[1,1,1,1,1,1,1,1,3,3,3,3,1],
+	[1,1,1,1,1,1,1,1,3,3,3,3,1]
+]
+
+var layer1 = [
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,0],
+	[1,1,1,1,1,1,1,1,1,1,1,1,0],
+	[1,1,1,1,1,1,1,1,1,1,1,0,0],
+	[1,1,1,1,1,1,4,1,1,1,0,0,0],
+	[1,1,1,1,0,0,4,1,1,1,0,0,0],
+	[1,4,1,1,0,0,0,4,4,1,0,0,0],
+	[0,4,1,0,0,0,0,4,4,0,0,0,1],
+	[0,0,4,0,0,0,0,0,4,0,0,0,1],
+	[0,0,0,0,0,0,0,0,0,0,0,1,1],
+	[0,0,0,0,0,0,0,0,0,0,0,1,1],
+	[0,0,0,0,0,0,0,0,0,0,1,1,1],
+	[0,0,0,0,0,0,0,0,0,0,1,1,1],
+	[0,0,0,0,0,0,0,0,0,1,1,1,1],
+	[0,0,0,0,0,0,0,0,0,1,1,1,1],
+	[0,0,0,0,0,0,0,0,0,1,1,1,1],
+	[0,0,0,0,0,0,0,0,0,0,1,1,1],
+	[0,0,0,0,0,0,0,0,0,0,1,1,1],
+	[0,0,0,0,0,0,0,0,0,0,1,1,1],
+	[0,0,0,0,0,0,0,0,0,1,1,1,0],
+	[0,0,0,0,0,0,0,0,0,1,1,1,0],
+	[0,0,0,0,0,0,0,0,0,0,1,4,0],
+	[0,0,0,0,0,0,0,0,0,0,0,4,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0]
+]
+
+var layer2 = [
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,1,1,1,1,1],
+	[1,1,1,1,1,1,1,1,4,1,1,1,1],
+	[1,1,1,1,1,1,1,4,4,1,1,1,1],
+	[1,1,1,1,1,1,1,0,4,4,1,1,1],
+	[1,1,1,0,1,1,0,0,4,4,1,1,4],
+	[1,1,1,0,1,1,0,0,0,4,4,1,4],
+	[0,1,4,0,1,0,0,0,0,4,4,4,0],
+	[0,0,4,0,0,0,0,0,0,0,4,4,0],
+	[0,0,0,0,0,0,0,0,0,0,4,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[0,0,0,0,0,0,0,0,0,0,0,0,1],
+	[0,0,0,0,0,0,0,0,0,0,0,1,1],
+	[0,0,0,0,0,0,0,0,0,0,0,1,1],
+	[0,0,0,0,0,0,0,0,0,0,0,1,1],
+	[0,0,0,0,0,0,0,0,0,0,0,4,1],
+	[0,0,0,0,0,0,0,0,0,0,0,4,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0]
+]
+
+var master = [];
+master[0] = layer0;
+master[1] = layer1;
+master[2] = layer2;
+
+var obj0 = [
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[15,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,16,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,15,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0]
+]
+
+var obj1 = [
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,7,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,6,5,0,0,0,0,0,0,0],
+	[0,0,0,6,0,0,0,0,0,0,0,0,0],
+	[0,0,0,6,0,0,0,0,0,0,0,0,0],
+	[0,0,6,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,16,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,5,0,0,0],
+	[0,0,0,0,0,0,0,0,0,5,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0]
+]
+
+var obj2 = [
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,15,0,0,0],
+	[0,0,0,0,0,15,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,15,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,5,0,0,0,0,0,0,0,0],
+	[0,0,0,0,5,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0]
+]
+
+var objMaster = [];
+objMaster[0] = obj0;
+objMaster[1] = obj1;
+objMaster[2] = obj2;
+
+//Object Arrays
+var sprites = [];
+var waters = [];
+var objSprites = [];
+var collisionArray = [[],[],[]];
+
+var waterState;
+
+//Magic Numbers
+var tileSize = 76;
+var halfTile = 38;
+var quarterTile = 18;
+
+//Map Size
+var mapX = layer0.length;
+var mapY = layer0[0].length;
+var mapZ = master.length;
+
+
+var player = null;
+var playerX = 0;
+var playerY = 0;
+var hit = false;
+var winLose = false;
+var mapPopulated = false;
+
+var up = false;
+var down = false;
+var right = false;
+var left = false;
+
+var canvas1 = document.getElementById("viewport1");
+var canv2d1 = canvas1.getContext("2d");
+var canX = canvas1.width;
+var canY = canvas1.height;
+
+var canvas2 = document.getElementById("viewport1");
+var canv2d2 = canvas2.getContext("2d");
+
+var image = new Image();
+image.src = "sprites.png";
+
+//Input
+window.addEventListener("keydown", function(event)
+{
+	switch(event.keyCode)
+	{
+		case 38:
+			up = true;
+			break;
+		
+		case 40:
+			down = true;
+			break;
+		
+		case 39:
+			right = true;
+			break;
+		
+		case 37:
+			left = true;
+			break;
+	}
+}, false);
+
+window.addEventListener("keyup", function(event)
+{
+	switch(event.keyCode)
+	{
+		case 38:
+			up = false;
+			break;
+		
+		case 40:
+			down = false;
+			break;
+		
+		case 39:
+			right = false;
+			break;
+		
+		case 37:
+			left = false;
+			break;
+	}
+}, false);
+
+//Objects
+var spriteObject =
+{
+	id: 0,
+	sourceX: 0,
+	sourceY: 0,
+	sourceWidth: tileSize,
+	sourceHeight: tileSize,
+	x: 0,
+	y: 0,
+	vx: 0,
+	vy: 0,
+	width: tileSize,
+	height: tileSize,
+	visible: true,
+	centerX: function()
+	{
+		return this.x + (this.width / 2);
+	},
+	centerY: function()
+	{
+		return this.y + (this.height / 2);
+	},
+	halfWidth: function()
+	{
+		return (this.width - 0) / 2;
+	},
+	halfHeight: function()
+	{
+		return (this.height - 0) / 2;
+	}
+};
+
+//Hit and Block Functions
+function hitCircle(c1, c2)
+{
+	var vx = c1.centerX() - c2.centerX();
+	var vy = c1.centerY() - c2.centerY();
+	var magnitude = Math.sqrt(vx * vx + vy * vy);
+	var totalRecal = c1.halfWidth() + c2.halfWidth();
+	var hit = magnitude < totalRecal;
+	return hit;
+}
+
+function hitRectangle(r1, r2)
+{
+	var hit = false;
+	var vx = r1.centerX() - r2.centerX();
+	var vy = r1.centerY() - r2.centerY();
+	var combinedHalfWidths = r1.halfWidth() + r2.halfWidth();
+	var combinedHalfHeights = r1.halfHeight() + r2.halfHeight();
+	if(Math.abs(vx) < combinedHalfWidths)
+	{
+		if(Math.abs(vy) < combinedHalfHeights)
+		{
+		hit = true;
+		}
+		else
+		{
+		hit = false;
+		}   
+	}
+	else
+	{
+		hit = false;
+	}
+	return hit;
+}
+
+function blockRectangle(r1, r2)
+{
+	var collisionSide = "";
+	var vx = r1.centerX() - r2.centerX();
+	var vy = r1.centerY() - r2.centerY();
+	var combinedHalfWidths = r1.halfWidth() + r2.halfWidth();
+	var combinedHalfHeights = r1.halfHeight() + r2.halfHeight();
+	if(Math.abs(vx) < combinedHalfWidths) 
+	{
+		if(Math.abs(vy) < combinedHalfHeights)
+		{
+			var overlapX = combinedHalfWidths - Math.abs(vx);
+			var overlapY = combinedHalfHeights - Math.abs(vy);
+			if(overlapX >=  overlapY)
+			{
+				if(vy > 0)
+				{
+					collisionSide = "top";
+					r1.y = r1.y + overlapY;
+				}
+				else 
+				{
+					collisionSide = "bottom";
+					r1.y = r1.y - overlapY;
+				}
+			} 
+			else 
+			{
+				if(vx > 0)
+				{
+					collisionSide = "left";
+					r1.x = r1.x + overlapX;
+				}
+				else 
+				{
+					collisionSide = "right";
+					r1.x = r1.x - overlapX;
+				}
+			} 
+		}
+		else 
+		{
+			collisionSide = "none";
+		}
+	} 
+	else 
+	{
+		collisionSide = "none";
+	}
+	return collisionSide;
+}
+
+//Start
+(function()
+{
+	console.log("Start");
+	reset();
+})();
+
+function reset()
+{
+	popMap();
+}
+
+update();
+function update()
+{
+	if(winLose == false)
+	{
+		playGame();
+		updateZIndex();
+	}
+	requestAnimationFrame(update, canvas1);
+	render();
+}
+
+setInterval(function(){ updateAnimation(); }, 1000);
+function updateAnimation()
+{
+	for(var i = 0; i < waters.length; i++)
+	{
+		var water = waters[i];
+		switch(waterState)
+		{
+			case 0:
+				water.sourceX = tileSize * 2;
+				break;
+				
+			case 1:
+				water.sourceX = tileSize * 3;
+				break;
+				
+			case 2:
+				water.sourceX = tileSize * 4;
+				break;
+				
+			case 3:
+				water.sourceX = tileSize * 5;
+				break;
+		}
+	}
+	if(waterState < 3)
+	{
+		waterState++;
+	}
+	else
+	{
+		waterState = 0;
+	}
+}
+
+function updateZIndex()
+{
+	for(var i = 0; i < objSprites.length; i++)
+	{
+		var obj = objSprites[i]
+		if(obj.y + obj.sourceHeight < player.y + player.sourceHeight)
+		{
+			obj.visible = false;
+		}
+		if(obj.y + obj.sourceHeight > player.y + player.sourceHeight)
+		{
+			obj.visible = true;
+		}
+	}
+}
+
+function popMap()
+{
+	var zAjust = 0;
+	var stagger;
+	for(var z = 0; z < mapZ; z++)
+	{
+		for(var y = 0; y < mapX; y++)
+		{	
+			for(var x = 0; x < mapY; x++)
+			{
+				var currentTileNum = master[z][y][x];
+				if(currentTileNum !== 0)
+				{
+					zAjust = z * halfTile;
+					if(y%2 == 0)
+					{
+						stagger = halfTile;
+					}
+					else
+					{
+						stagger = 0;
+					}
+					switch(currentTileNum)
+					{
+						case 1:
+							var grass = Object.create(spriteObject);
+							grass.sourceX = 0;
+							grass.sourceY = 0;
+							grass.x = (x * tileSize - stagger);
+							grass.y = (y * quarterTile) - zAjust;
+							sprites.push(grass);
+							collisionArray[z].push(grass);
+							break;
+							
+						case 2:
+							var dirt = Object.create(spriteObject);
+							dirt.sourceX = tileSize;
+							dirt.sourceY = 0;
+							dirt.x = (x * tileSize - stagger);
+							dirt.y = (y * quarterTile) - zAjust;
+							sprites.push(dirt);
+							collisionArray[z].push(dirt);
+							break;
+							
+						case 3:
+							var water = Object.create(spriteObject);
+							water.sourceX = tileSize * 2;
+							water.sourceY = 0;
+							water.x = (x * tileSize - stagger);
+							water.y = (y * quarterTile) - zAjust;
+							sprites.push(water);
+							waters.push(water);
+							collisionArray[z].push(water);
+							break;
+							
+						case 4:
+							var grasshalf = Object.create(spriteObject);
+							grasshalf.sourceX = 0;
+							grasshalf.sourceY = tileSize;
+							grasshalf.x = (x * tileSize - stagger);
+							grasshalf.y = (y * quarterTile) - zAjust;
+							sprites.push(grasshalf);
+							collisionArray[z].push(grasshalf);
+							break;
+					}
+				}
+			}
+		}
+	}
+	var zAjust = 0;
+	var stagger;
+	for(var z = 0; z < mapZ; z++)
+	{
+		for(var y = 0; y < mapX; y++)
+		{	
+			for(var x = 0; x < mapY; x++)
+			{
+				var currentTileNum = objMaster[z][y][x];
+				if(currentTileNum !== 0)
+				{
+					zAjust = z * halfTile;
+					if(y%2 == 0)
+					{
+						stagger = halfTile;
+					}
+					else
+					{
+						stagger = 0;
+					}
+					switch(currentTileNum)
+					{
+						case 5:
+							var fenceleft = Object.create(spriteObject);
+							fenceleft.sourceX = 92;
+							fenceleft.sourceY = tileSize;
+							fenceleft.width = 42;
+							fenceleft.height = 48;
+							fenceleft.sourceWidth = 42;
+							fenceleft.sourceHeight = 48;
+							fenceleft.x = (x * tileSize - stagger) + 18;
+							fenceleft.y = (y * quarterTile) - zAjust - 18;
+							sprites.push(fenceleft);
+							var fenceleft2 = $.extend(true, {}, fenceleft);
+							objSprites.push(fenceleft2);
+							collisionArray[z].push(fenceleft);
+							break;
+						
+						case 6:
+							var fenceright = Object.create(spriteObject);
+							fenceright.sourceX = 174;
+							fenceright.sourceY = tileSize;
+							fenceright.width = 42;
+							fenceright.height = 48;
+							fenceright.sourceWidth = 42;
+							fenceright.sourceHeight = 48,
+							fenceright.x = (x * tileSize - stagger) + 18;
+							fenceright.y = (y * quarterTile) - zAjust - 18;
+							sprites.push(fenceright);
+							var fenceright2 = $.extend(true, {}, fenceright);
+							objSprites.push(fenceright2);
+							collisionArray[z].push(fenceright);
+							break;
+							
+						case 15:
+							var greentree = Object.create(spriteObject);
+							greentree.sourceX = 0;
+							greentree.sourceY = 328;
+							greentree.width = 102;
+							greentree.height = 128;
+							greentree.sourceWidth = 102,
+							greentree.sourceHeight = 128,
+							greentree.x = (x * tileSize - stagger + 22);
+							greentree.y = (y * quarterTile) - zAjust - 10;
+							sprites.push(greentree);
+							var greentree2 = $.extend(true, {}, greentree);
+							objSprites.push(greentree2);
+							collisionArray[z].push(greentree);
+							break;
+						
+						case 16:
+							var tree = Object.create(spriteObject);
+							tree.sourceX = 102;
+							tree.sourceY = 328;
+							tree.width = 112;
+							tree.height = 128;
+							tree.sourceWidth = 112;
+							tree.sourceHeight = 128;
+							tree.x = (x * tileSize - stagger + 16);
+							tree.y = (y * quarterTile) - zAjust - 8;
+							sprites.push(tree);
+							var tree2 = $.extend(true, {}, tree);
+							objSprites.push(tree2);
+							collisionArray[z].push(tree);
+							break;
+					}
+				}
+			}
+		}
+	}
+	
+	player = Object.create(spriteObject);
+	player.sourceX = 0;
+	player.sourceY = tileSize * 2;
+	player.x = (7 * tileSize);
+	player.y = (7 * quarterTile);
+	sprites.push(player);
+	
+	console.log("mapPopulated");
+	mapPopulated = true;
+}
+
+function playGame()
+{
+	//Player Movement
+	if(up && !down)
+	{
+		player.vy = -.48;
+	}
+	if(down && !up)
+	{	
+		player.vy = .48;
+	}
+	if(left && !right)
+	{
+		player.vx = -1;
+	}
+	if(right && !left)
+	{	
+		player.vx = 1;
+	}
+	if(!up && !down)
+	{
+		player.vy = 0;
+	}
+	if(!left && !right)
+	{
+		player.vx = 0;
+	}
+	player.x += player.vx;
+	player.y += player.vy;
+	
+	//Flip Character 
+	if(player.vx < 0)
+	{
+		player.sourceX = 0;
+	}
+	if(player.vx > 0)
+	{
+		player.sourceX = tileSize;
+	}
+	
+	var playerX = Math.floor(player.x / tileSize);
+	var playerY = Math.floor(player.y / quarterTile);
+	var playerLevel = 2;
+	for(var i = 0; i < collisionArray[playerLevel].length; i++)
+	{
+		var currentTile = collisionArray[playerLevel][i];
+		var collisionX = Math.floor(currentTile.x / tileSize);
+		var collisionY = Math.floor(currentTile.y / quarterTile);
+		if(collisionY == playerY && collisionX  == playerX)
+		{
+			blockRectangle(player, currentTile)
+		}
+	}
+	
+	
+	//Screen Bounding Box
+	player.x = Math.max(0, Math.min(player.x + player.vx, canvas1.width-player.width - 0));
+	player.y = Math.max(0, Math.min(player.y + player.vy, canvas1.height-player.height - 0));
+}
+
+//NOT FUNTIONAL
+/*function gameOver()
+{
+	end = Object.create(spriteObject);
+	end.sourceY = 132;
+	end.sourceWidth = 128;
+	end.sourceHeight = 64;
+	end.width = 128;
+	end.height = 64;
+	end.x = canvas1.width / 2 - end.width /2;
+	end.y = canvas1.height / 2 - end.height /2;
+	sprites.push(end);
+}
+
+function gameWin()
+{
+	win = Object.create(spriteObject);
+	win.sourceY = 64;
+	win.sourceWidth = 128;
+	win.sourceHeight = 64;
+	win.width = 128;
+	win.height = 64;
+	win.x = canvas1.width / 2 - win.width /2;
+	win.y = canvas1.height / 2 - win.height /2;
+	sprites.push(win);
+}*/
+
+function render()
+{
+	if(sprites.length !== 0)
+	{
+		for(var i = 0; i < sprites.length; i++)
+		{
+			var sprite = sprites[i];
+			if(sprite.visible)
+			{
+				canv2d1.drawImage(image, sprite.sourceX, sprite.sourceY, sprite.sourceWidth, sprite.sourceHeight, Math.floor(sprite.x), Math.floor(sprite.y), sprite.width, sprite.height);
+			}
+		}
+	}
+	
+	if(objSprites.length !== 0)
+	{
+		for(var i = 0; i < objSprites.length; i++)
+		{
+			var sprite2 = objSprites[i];
+			if(sprite2.visible)
+			{
+				canv2d2.drawImage(image, sprite2.sourceX, sprite2.sourceY, sprite2.sourceWidth, sprite2.sourceHeight, Math.floor(sprite2.x), Math.floor(sprite2.y), sprite2.width, sprite2.height);
+			}
+		}
+	}
+}
